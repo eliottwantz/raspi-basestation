0045c5806e3566d7e1f4b3eae6f6e88cf6fd4856
Eliott Wantz <93885811+DevEliott@users.noreply.github.com>
2022-09-18 11:37:49 +0000

    full exemple working

--- client/client.cc
+++ client/client.cc
@@ -8,6 +8,7 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <cstring>
+#include <iostream>
 
 #include "sensor.pb.h"
 #include "sensorstate.pb.h"
@@ -42,6 +43,14 @@ int main()
 
     main_computer->set_state(pb::MainComputer_States_ACCELERATING);
     brake_manager->set_state(pb::BrakeManager_States_BRAKING);
+    brake_manager->set_hydrolic_pressure_loss(false);
+    brake_manager->set_critical_pod_acceleration_mesure_timeout(true);
+    brake_manager->set_critical_pod_deceleration_instruction_timeout(false);
+    brake_manager->set_verin_blocked(false);
+    brake_manager->set_emergency_valve_open_without_hydrolic_pressor_diminution(false);
+    brake_manager->set_critical_emergency_brakes_without_deceleration(false);
+    brake_manager->set_mesured_distance_less_than_desired(false);
+    brake_manager->set_mesured_distance_greater_as_desired(true);
 
     sensor_state->set_allocated_main_computer(main_computer);
     sensor_state->set_allocated_brake_manager(brake_manager);
